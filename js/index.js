6b224e2a961023e4f61a98be31d0eeeaea2604a6
Daniel Peixoto Pinto da Silva <peixoto.daniel@hotmail.com>
2022-05-15 18:30:41 -0300

    Mais css

---
+++ js/index.js
@@ -0,0 +1,22 @@
+function selectElement(e) {
+  return document.querySelector(e);
+}
+
+function selectAllElement(e) {
+  return document.querySelectorAll(e);
+}
+
+const priceTypes = selectAllElement('.pricing__types > span');
+const plans = selectAllElement('.plan');
+
+function handleSwitchToggle() {
+  priceTypes.forEach((type) => type.classList.toggle('pricing__type--active'));
+
+  plans.forEach((plan) => {
+    plan.classList.toggle('plan--best');
+    const planPrice1 = plan.querySelector('.plan__price:nth-child(2)');
+    const planPrice2 = plan.querySelector('.plan__price:nth-child(3)');
+    planPrice1.classList.toggle('plan__price--hidden');
+    planPrice2.classList.toggle('plan__price--hidden');
+  });
+}
